8f2c00ba62c4ce8c89c1f209c24a7c13fa9dd5a8
rdseveryukhin <rdseveryukhin1996@mail.ru>
2023-03-05 19:10:59 +0100

    Добавьте файлы проекта.

---
+++ SlidingWindow/SlidingWindow.cpp
@@ -0,0 +1,18 @@
+﻿#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxSumOfSubarray(vector<int> arr, int k) {
+    int maxSum = 0;
+    int runningSum = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+        runningSum += arr[i];
+
+        if (i >= k - 1) {
+            maxSum = max(runningSum, maxSum);
+            runningSum -= arr[i - (k - 1)];
+        }
+    }
+    return maxSum;
+}
