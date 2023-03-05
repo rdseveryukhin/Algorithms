ea7d9816cad38fc89ca2ec0e17e5890137befdb0
rdseveryukhin <rdseveryukhin1996@mail.ru>
2023-03-05 19:10:59 +0100

    Добавьте файлы проекта.

---
+++ ListReverse/ListReverse.cpp
@@ -0,0 +1,31 @@
+﻿#include<vector>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) return head;
+
+        ListNode* prev = nullptr;
+        ListNode* temp = nullptr;
+        ListNode* current = head;
+
+        while (current != nullptr) {
+            temp = current->next;
+            current->next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+};
