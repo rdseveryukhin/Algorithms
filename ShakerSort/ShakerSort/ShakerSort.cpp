ec8767184a776799cff3722b9d2138d21b43b791
rdseveryukhin <rdseveryukhin1996@mail.ru>
2023-03-05 19:10:59 +0100

    Добавьте файлы проекта.

---
+++ ShakerSort/ShakerSort/ShakerSort.cpp
@@ -0,0 +1,30 @@
+﻿#include <vector>
+
+class ShakeSort {
+public:
+    void Sort(std::vector<int>& nums) {
+        if (nums.empty()) { return; }
+        int left = 0;
+        int right = nums.size() - 1;
+        int tmp = 0;
+        while (left <= right) {
+            for (int i = right; i > left; i--) {
+                if (nums[i] < nums[i - 1]) {
+                    tmp = nums[i];
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = tmp;
+                }
+            }
+            left++;
+
+            for (int i = left; i < right; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    tmp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = tmp;
+                }
+            }
+            right--;
+        }
+     }
+};
