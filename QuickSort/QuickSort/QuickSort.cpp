d7a2a0d500ee4e2e40358b9f4479d8a156292403
rdseveryukhin <rdseveryukhin1996@mail.ru>
2023-03-05 19:10:59 +0100

    Добавьте файлы проекта.

---
+++ QuickSort/QuickSort/QuickSort.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+using namespace std;
+
+class QuickSort {
+public:
+    void Sort(vector<int>& nums) {
+        if (!nums.empty()) {
+            QuickSortImpl(nums, 0, nums.size() - 1);
+        }
+    }
+
+    void QuickSortImpl(vector<int>& nums, int l, int r) {
+        if (l < r) {
+            int q = Partition(nums, l, r);
+            QuickSortImpl(nums, l, q - 1);
+            QuickSortImpl(nums, q + 1, r);
+        }
+    }
+
+    int Partition(vector<int>& nums, int l, int r) {
+        int x = nums[r];
+        int less = l;
+
+        for (int i = l; i < r; ++i) {
+            if (nums[i] <= x) {
+                swap(nums[i], nums[less]);
+                ++less;
+            }
+        }
+        swap(nums[less], nums[r]);
+        return less;
+    }
+};
+
