c8234ca23aa7a4640db109eb54055b2b38ed32ab
rdseveryukhin <rdseveryukhin1996@mail.ru>
2023-03-05 19:10:59 +0100

    Добавьте файлы проекта.

---
+++ BubbleSort/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+
+class BubbleSort {
+public:
+    void Sort(std::vector<int>& nums) {
+        int tmp = 0;
+        for (int i = 0; i + 1 < nums.size(); i++) {
+            for (int j = 0; j + 1 < nums.size() - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    tmp = nums[j + 1];
+                    nums[j + 1] = nums[j];
+                    nums[j] = tmp;
+                }
+
+            }
+        }
+    }
+};
+
